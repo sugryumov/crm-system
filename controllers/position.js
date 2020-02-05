1fcc28d481393826de62c0b74b5794a3b87896d2
Sergey <snugryumov@gmail.com>
2020-02-05 14:07:38 +0300

    features: position routes

--- controllers/position.js
+++ controllers/position.js
@@ -1,7 +1,59 @@
-module.exports.getByCategoryId = (req, res) => {};
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
-module.exports.create = (req, res) => {};
+module.exports.getByCategoryId = async (req, res) => {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      user: req.user.id
+    });
 
-module.exports.remove = (req, res) => {};
+    res.status(200), json(positions);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
 
-module.exports.update = (req, res) => {};
+module.exports.create = async (req, res) => {
+  try {
+    const position = new Position({
+      name: req.body.name,
+      cost: req.body.cost,
+      category: req.body.category,
+      user: req.user.id
+    });
+
+    await position.save();
+    res.status(201).json(position);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
+
+module.exports.remove = async (req, res) => {
+  try {
+    await Position.remove({
+      _id: req.params.id
+    });
+
+    res.status(200).json({
+      message: 'Позиция удалена'
+    });
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
+
+module.exports.update = async (req, res) => {
+  try {
+    const position = await Position.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: req.body },
+      { new: true }
+    );
+
+    res.status(200).json(position);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
