5ab5d7e79c1f099035c0b3b54644a728bd25abd7
Sergey <snugryumov@gmail.com>
2020-02-05 14:08:37 +0300

    features: category routes

--- controllers/category.js
+++ controllers/category.js
@@ -1,13 +1,75 @@
-module.exports.getAll = (req, res) => {
-	res.json({
-		message: 'Categories...'
-	})
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.find({
+      user: req.user.id
+    });
+
+    res.status(200).json(categories);
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
 
-module.exports.getById = (req, res) => {};
+module.exports.getById = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
 
-module.exports.remove = (req, res) => {};
+    res.status(200).json(category);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
 
-module.exports.create = (req, res) => {};
+module.exports.remove = async (req, res) => {
+  try {
+    await Category.remove({ _id: req.params.id });
+    await Position.remove({ category: req.params.id });
 
-module.exports.update = (req, res) => {};
+    res.status(200).json({
+      message: 'Категория удалена'
+    });
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
+
+module.exports.create = async (req, res) => {
+  try {
+    const category = new Category({
+      name: req.body.name,
+      user: req.user.id,
+      imageSrc: req.file ? req.file.path : ''
+    });
+
+    await category.save();
+    res.status(201).json(category);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
+
+module.exports.update = async (req, res) => {
+  const updated = {
+    name: req.body.name
+  };
+
+  if (req.file) {
+    updated.imageSrc = req.file.path;
+  }
+
+  try {
+    const category = await Category.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: updated },
+      { new: true }
+    );
+
+    res.status(200).json(category);
+  } catch (err) {
+    errorHandler(res, err);
+  }
+};
